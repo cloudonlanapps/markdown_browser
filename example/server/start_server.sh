2024fcfc85f64c72224327517ca1b20f2f79a1c7
Ananda S <4833102+asarangaram@users.noreply.github.com>
2023-10-19 18:52:11 +0530

    update server to serve on network ip

--- example/server/start_server.sh
+++ example/server/start_server.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-V_ENV=.venv
+V_ENV=".venv"
 
 if [ ! -d "$V_ENV" ]; then
     echo "$V_ENV does not exist. Creating"
     python3 -m venv .venv
 fi
 
-source "$V_ENV/bin/activate"
+source "${V_ENV}/bin/activate"
 pip install flask
-python ./server.py
+flask --app server.py run -h 0.0.0.0
 deactivate
